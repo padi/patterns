ab49e97a01e58347ba4b5b0b9356e0bfac97aae0
Marc Rendl Ignacio <marcrendlignacio@gmail.com>
2017-08-17 04:13:45 +0800

    Own solution to :after_filter

--- lib/filtering.rb
+++ lib/filtering.rb
@@ -8,13 +8,22 @@ module Filtering
       before_actions << method
     end
 
+    def after_action(method)
+      after_actions << method
+    end
+
     def before_actions
       @before_actions ||= []
+    end
+
+    def after_actions
+      @after_actions ||= []
     end
   end
 
   def process(action_name)
     self.class.before_actions.each { |method| send method }
     super
+    self.class.after_actions.each { |method| send method }
   end
 end
