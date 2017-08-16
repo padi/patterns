090205aa6cf5775a0ae53a603633f03ee0829ebc
Marc Rendl Ignacio <marcrendlignacio@gmail.com>
2017-08-17 04:13:45 +0800

    Own solution to :after_filter

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,17 @@
 class HomeController < ApplicationController
   before_action :header
 
+  after_action :footer
+
   def index
     response.write "hi from HomeController"
   end
 
   def header
     response.write "<h1>My App</h1>"
+  end
+
+  def footer
+    response.write "&copy; me"
   end
 end
